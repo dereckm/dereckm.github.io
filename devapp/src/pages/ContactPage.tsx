19c08c4947b65e5a19db2b29fe9fbe82a0e89536
Dereck Melancon <ak90730@ens.etsmtl.ca>
2025-03-14 02:47:27 +0000

    improvements + new build

--- devapp/src/pages/ContactPage.tsx
+++ devapp/src/pages/ContactPage.tsx
@@ -2,6 +2,9 @@ const ContactPage = () => {
     return (
         <div>
             <h1 id='contact'>Contact</h1>
+            <p>Always happy to connect to discuss technology.</p>
+            <p>Email: <a href="mailto:dereck.melancon@gmail.com">dereck.melancon@gmail.com</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/dereck-melancon/">Dereck Melancon - B. Eng.</a></p>
         </div>
     )
 }
